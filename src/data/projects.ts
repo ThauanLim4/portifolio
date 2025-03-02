f7cf8599fa2729c856c848e5a8bab4a7a3630943
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-03-02 09:13:03 -0300

    FEAT: adicionado novo projeto "TheMovieDB" e atualizado informações de projetos existentes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,7 @@ import byteStoreImg from "../img/byteStore.png";
 import lojaTenis from "../img/lojaTenis.png";
 import myfood from "../img/myfood.png";
 import development from '../img/development.svg';
+import themoviedb from '../img/themoviedb.png';
 import { StaticImageData } from "next/image";
 
 export type Project = {
@@ -15,25 +16,31 @@ export type Project = {
 
 export const Projects: Project[] = [
     {
-        id: 0, nomeDoProjeto: "Myfood", imagem: myfood, tecnologiasUsadas: ["react", "next.js", "tailwindcss"], descricao: "Projeto de um site de delivery de comidas, baseado no ifood e seus semelhantes", link: "https://myfood-mu.vercel.app/"
+        id: 0, nomeDoProjeto: "TheMovieDB", imagem: themoviedb, tecnologiasUsadas: ["react", "next.js", "tailwindcss"], descricao: "Projeto de um site de filmes e series, baseado no themoviedb e imdb, o projeto foi construido utilizando a api do themoviedb", link: "https://themvdb-pmdaz2jdi-thauanlim4s-projects.vercel.app/"
+    },
+
+    {
+        id: 1, nomeDoProjeto: "Myfood", imagem: myfood, tecnologiasUsadas: ["react", "next.js", "tailwindcss"], descricao: "Projeto de um site de delivery de comidas, baseado no ifood e seus semelhantes", link: "https://myfood-mu.vercel.app/"
     },
 
     {
         id: 2, nomeDoProjeto: "ByteStore", imagem: byteStoreImg, tecnologiasUsadas: ["html", "css", "javascript"], descricao: "Pagina de vendas simples de produtos do ramo de tecnologia", link: "https://vercel.live/link/landing-page-tec.vercel.app?via=deployment-domains-list&p=1"
     },
 
+
+
     {
-        id: 3, nomeDoProjeto: "TenisStore", imagem: lojaTenis, tecnologiasUsadas: ["html, css, javascript"], descricao: "Pagina de vendas de tênis bem simples, um dos meus primeiros projetos", link: "https://loja-tenis-azure.vercel.app/"
+        id: 4, nomeDoProjeto: "TenisStore", imagem: lojaTenis, tecnologiasUsadas: ["html, css, javascript"], descricao: "Pagina de vendas de tênis bem simples, um dos meus primeiros projetos", link: "https://loja-tenis-azure.vercel.app/"
     }
 ]
 
 export const ProjectInDevelopment: Project[] = [
     {
         id: 1,
-        nomeDoProjeto: "TheMDB",
+        nomeDoProjeto: "???",
         imagem: development,
-        tecnologiasUsadas: ["react", "next.js", "typescript", "tailwindcss"],
-        descricao: "Projeto de um site de filmes e series, baseado no themoviedb",
+        tecnologiasUsadas: ["??", "??", "??"],
+        descricao: "No momento nenhum projeto em desenvolvimento",
         link: " "
     }
 ]
